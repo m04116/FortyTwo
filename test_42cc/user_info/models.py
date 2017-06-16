86c2b3edcb7ec1758ecbec960aacafd19d6d5e49
Konstantin <krodinv@gmail.com>
2017-06-16 20:53:06 +0300

    Template, requirements, models, views, urls

--- test_42cc/user_info/models.py
+++ test_42cc/user_info/models.py
@@ -1,3 +1,12 @@
+from datetime import date, datetime
 from django.db import models
 
-# Create your models here.
+class UserInformation(models.Model):
+    name = models.CharField(max_length=100,)
+    last_name = models.CharField(max_length=100,)
+    date_of_birth = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True, null=True, default=None)
+    email = models.EmailField()
+    jabber = models.EmailField()
+    skype = models.CharField(max_length=50,)
+    other_contacts = models.TextField(blank=True, null=True, default=None)
--- test_42cc/user_info/views.py
+++ test_42cc/user_info/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import UserInformation
 
-# Create your views here.
+def info(request):
+    # user = UserInformation.objects.get(id=1)
+    # context = {'user': user}
+    # return render(request, 'user_info/user_data.html', context)
+    return render(request, 'user_info/user_data.html')
