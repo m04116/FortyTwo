339fc37bc1d0b42e736a711a8877045cca33f802
Konstantin <krodinv@gmail.com>
2017-06-17 11:43:29 +0300

    template without hardcode

--- test_42cc/user_info/views.py
+++ test_42cc/user_info/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import UserInformation
 
 def info(request):
-    # user = UserInformation.objects.get(id=1)
-    # context = {'user': user}
-    # return render(request, 'user_info/user_data.html', context)
-    return render(request, 'user_info/user_data.html')
+    user = UserInformation.objects.get(id=1)
+    context = {'user': user}
+    return render(request, 'user_info/user_data.html', context)
+    
